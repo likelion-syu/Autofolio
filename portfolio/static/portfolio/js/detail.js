6a979e75b872468dae0ca3e7cbab455e5761fae5
Yoo KyeongSoo <dev.yoogomja@gmail.com>
2019-08-07 22:45:27 +0900

    moment added
    Signed-off-by: Yoo KyeongSoo <dev.yoogomja@gmail.com>

---
+++ portfolio/static/portfolio/js/detail.js
@@ -0,0 +1,11 @@
+window.__angular_app.controller('pDetailCtrl' , ['$scope' , function($s){
+    $s.md = {
+
+    }
+    $s.fn = {
+        init : function(){
+
+        }
+    }
+    $s.fn.init();
+}]);
