4eafe74d2dd22118a52eb8a5c69937850107ba19
Yoo KyeongSoo <dev.yoogomja@gmail.com>
2019-08-07 21:59:08 +0900

    portfolio author added in models
    Signed-off-by: Yoo KyeongSoo <dev.yoogomja@gmail.com>

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from draft.models import Draft , DraftActivity , DraftPortfolio , DraftResume
+from .models import Portfolio , Subdomain
 
 def portfolio_list(req):
     # 현재 생성된 draft의 수를 확인
     # drafts_count = Draft.objects.filter(author=req.user).count()
+    portfolios = Portfolio.objects.all().filter(author=req.user)
     
     return render(req , './portfolio/list.html' , {
         'drafts_count' : 0,
