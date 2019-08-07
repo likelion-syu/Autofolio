09d7c342d7ca0eacf46b98bd9ca0683192ae4a31
blair06 <hyewon980610@likelion.org>
2019-08-07 19:25:20 +0900

    addDraft

--- autofolio/urls.py
+++ autofolio/urls.py
@@ -1,13 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from django.urls import path, include
-
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('draft/', include('draft.urls')),
-=======
 from django.urls import path , include
 import account.views
 
--- draft/urls.py
+++ draft/urls.py
@@ -1,17 +1,10 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from django.urls import path
-from . import views
-
-urlpatterns = [
-   path('create', views.draft, name='draft')
-]
-=======
-from django.urls import path , include
 from . import views
 
 urlpatterns = [
     path('list' , views.draft_list , name="draft_list"),
 	path('detail' , views.draft_detail , name="draft_detail"),
+    path('create', views.draft, name='draft')
+    
 ]
->>>>>>> d360ac2e76473f663c2c05c35b60b8a78cd14e17
