d360ac2e76473f663c2c05c35b60b8a78cd14e17
Yoo KyeongSoo <dev.yoogomja@gmail.com>
2019-08-07 19:09:06 +0900

    portfolio error fixed
    Signed-off-by: Yoo KyeongSoo <dev.yoogomja@gmail.com>

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 from draft.models import Draft , DraftActivity , DraftPortfolio , DraftResume
 
 def portfolio_list(req):
-    drafts_count = Draft.objects().filter()
+    # 현재 생성된 draft의 수를 확인
+    # drafts_count = Draft.objects.filter(author=req.user).count()
     
-    return render(req , './portfolio/list.html')	
+    return render(req , './portfolio/list.html' , {
+        'drafts_count' : 0,
+    })	
 
 def portfolio_detail(req):
     return render(req , './portfolio/detail.html')
