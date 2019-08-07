d330b85d9bc6fa885bad7d7e0ad3c35df2097018
Yoo KyeongSoo <dev.yoogomja@gmail.com>
2019-08-07 19:44:59 +0900

    swiper added
    Signed-off-by: Yoo KyeongSoo <dev.yoogomja@gmail.com>

--- portfolio/static/portfolio/js/list.js
+++ portfolio/static/portfolio/js/list.js
@@ -1,14 +1,37 @@
 angular.module('afapp' , [])
+.directive('myRepeatDirective', function() {
+    return function(scope, element, attrs) {
+        if (scope.$last){
+        // window.alert("im the last!");
+            // window._swiper = 
+            window._swiper = new Swiper('.swiper-container', {
+                slidesPerView: 3,
+                spaceBetween: 30,
+                pagination: {
+                  el: '.swiper-pagination',
+                  clickable: true,
+                },
+              });
+        }
+    };
+})
 .controller("pListCtrl" , ['$scope' , ($s) =>{
     console.log('Hello');
 
     $s.md = {
-
+        items : [
+            { thumbnail : 'thumbnail' , title : 'title' , created_dt : (new Date()) },
+            { thumbnail : 'thumbnail' , title : 'title' , created_dt : (new Date()) },
+            { thumbnail : 'thumbnail' , title : 'title' , created_dt : (new Date()) },
+            { thumbnail : 'thumbnail' , title : 'title' , created_dt : (new Date()) }
+        ],
+        swiper : {}
     }
 
     $s.fn = {
         init : function(){
             console.log('initialized');
+
         }
     }
 
