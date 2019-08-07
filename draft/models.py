b41970f8e03cfaf1fb5d4dad40b337b7463342d8
Yoo KyeongSoo <dev.yoogomja@gmail.com>
2019-08-07 15:13:00 +0900

    draft 주석 추가
    Signed-off-by: Yoo KyeongSoo <dev.yoogomja@gmail.com>

--- draft/models.py
+++ draft/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User , Group
 
-
-# Create your models here.
-
+# 파일 저장 관련 주소생성 함수 
 def directory_path_by_user(instance , filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
     now = timezone.now()
     return '{0}/user_{1}/{2}'.format(now.strftime('%Y%m') , instance.user_id.id , filename)
 
+
+# 드래프트 전체를 관리하는 모델
 class Draft(models.Model):
     title = models.CharField(max_length=200)
     created_dt = models.DateTimeField(default=timezone.now)
     last_modified_dt = models.DateTimeField(default=timezone.now)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     
+# 드래프트 중 이력서 항목 관리용 모델
 class DraftResume(models.Model):
     draft_id = models.ForeignKey(Draft , on_delete=models.CASCADE , default=None)
     name = models.CharField(max_length=50)
@@ -23,6 +24,7 @@ class DraftResume(models.Model):
     created_dt = models.DateTimeField(default=timezone.now)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
+# 드래프트 중 포트폴리오 항목 관리용 모델
 class DraftPortfolio(models.Model):
     draft_id = models.ForeignKey(Draft , on_delete=models.CASCADE )
     title = models.CharField(max_length=200)
@@ -37,6 +39,7 @@ class DraftPortfolio(models.Model):
     created_dt = models.DateTimeField(default=timezone.now)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
+# 드래프트 중 활동 내역 항목 관리용 모델
 class DraftActivity(models.Model):
     draft_id = models.ForeignKey(Draft , on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
