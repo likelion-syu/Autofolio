6b505363b59f9eec2a096f6dcd29a6e1789764d5
Yoo KyeongSoo <dev.yoogomja@gmail.com>
2019-08-08 16:14:14 +0900

    해시태그 기능 추가
    Signed-off-by: Yoo KyeongSoo <dev.yoogomja@gmail.com>

--- portfolio/static/portfolio/js/detail.js
+++ portfolio/static/portfolio/js/detail.js
@@ -1,10 +1,46 @@
-window.__angular_app.controller('pDetailCtrl' , ['$scope' , function($s){
+window.__angular_app
+.directive('krInput', [ '$parse', function($parse) { return { priority : 2, restrict : 'A', compile : function(element) { element.on('compositionstart', function(e) { e.stopImmediatePropagation(); }); }, }; } ])
+.controller('pDetailCtrl' , ['$scope' , function($s){
     $s.md = {
-
+        tags : {
+            list : [
+                'tag1',
+                'tag2',
+            ],
+            input : ""
+        }
     }
     $s.fn = {
+        tags : {
+            init : function(){},
+            remove : function(idx){
+                // console.log(idx);
+                // console.log($s.md.tags.list);
+                $s.md.tags.list.splice(idx , 1);
+            },
+            insert : function(e){
+                // console.log(e);
+                if(e.keyCode === 13){
+                    if($s.md.tags.input.trim() !== ""){
+                        if($s.md.tags.list.length >= 5){
+                            alert('태그는 5개 이상 등록할 수 없습니다.');
+                            $s.md.tags.input = "";
+                        }
+                        else if($s.md.tags.input.indexOf(" ") >= 0){
+                            alert("태그에는 공백이 입력될 수 없습니다.");
+                        }
+                        else{
+                            $s.md.tags.list.push(
+                                $s.md.tags.input.trim()
+                            );
+                            $s.md.tags.input = "";
+                        }
+                    }
+                }
+            },
+        },
         init : function(){
-
+            console.log($s.md.tags);
         }
     }
     $s.fn.init();
