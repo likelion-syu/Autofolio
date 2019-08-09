9a3ab754723a0c9da05aacb59f63c647577fe34e
seonghyeon <tjdgusdkf@gmail.com>
2019-08-09 15:13:00 +0900

    index gogo

--- account/urls.py
+++ account/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 urlpatterns = [
     path('' , views.index , name="home"),
-    path('signin' , views.signin , name="signin"),
-    path('signup' , views.signup , name="signup"),
-    path('find' , views.account_find , name="account_find"),
-    path('user' , views.account_detail , name="account_detail"),
-    path('edit/user' , views.account_edit , name="account_edit"),
+    path('signin', views.signin , name="signin"),
+    path('signup', views.signup , name="signup"),
+    path('logout', views.logout , name="logout"),
+    path('find', views.account_find , name="account_find"),
+    path('user', views.account_detail , name="account_detail"),
+    path('edit/user', views.account_edit , name="account_edit"),
 ]
