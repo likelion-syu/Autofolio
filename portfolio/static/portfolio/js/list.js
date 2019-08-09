c35ba1861a5e2771c3fcd0f2513e5ccdd7776019
Yoo KyeongSoo <dev.yoogomja@gmail.com>
2019-08-09 11:13:56 +0900

    포트폴리오 리스트 디자인 종료
    Signed-off-by: Yoo KyeongSoo <dev.yoogomja@gmail.com>

--- portfolio/static/portfolio/js/list.js
+++ portfolio/static/portfolio/js/list.js
@@ -13,26 +13,70 @@ window.__angular_app
         }
     };
 })
+.directive('runPerRepeating' , function(){
+    return {
+        scope: { 
+            runPerRepeating : '=' ,
+            runPerRepeatingSource : '='
+        },
+        link : (scope , elem , attrs) =>{
+            scope.runPerRepeating(scope.runPerRepeatingSource);
+            
+        } 
+    }
+})
 .controller("pListCtrl" , ['$scope' , ($s) =>{
-    console.log('Hello');
-
     $s.md = {
-        items : [
-            { thumbnail : 'thumbnail' , title : 'title' , created_dt : (new Date()) },
-            { thumbnail : 'thumbnail' , title : 'title' , created_dt : (new Date()) },
-            { thumbnail : 'thumbnail' , title : 'title' , created_dt : (new Date()) },
-            { thumbnail : 'thumbnail' , title : 'title' , created_dt : (new Date()) }
-        ],
-        swiper : {}
+        portfolios : {
+            items : []
+        },
+        drafts : {
+            count : 0
+        }
     }
 
     $s.fn = {
         init : function(){
-            console.log('initialized');
+            if($s.md.drafts.count === 0){
+                $('#btn-modal-toggle').click();
+            }
+        },
+        list : {
+            close : function(){
 
+            },
+            repeat : function(item){
+                if(item.fields && item.fields.tags){
+                    let splitted = item.fields.tags.split(',');
+                    item.tags_splitted = splitted;
+                }
+                else{
+                    item.tags_splitted = [];
+                }
+            }
         }
     }
 
+    $s.ext = {
+        init : function(){
+            $s.md.portfolios.items = window.__model.portfolios_serialized;
+            $s.md.drafts.count = window.__model.drafts_count;
+            $s.$apply();    
 
-    $s.fn.init();
-}]);
+            $s.fn.init();
+        }
+    }
+}])
+.filter('momentFromNow', function () {
+    return function (item) {
+        let currentMoment = moment(item);
+        // console.log(item , currentMoment);
+        return currentMoment.fromNow();
+    };
+});
+
+window.__angular ={
+    init : function(){
+        angular.element($('.portfolio-list').eq(0)).scope().ext.init();
+    }
+}
