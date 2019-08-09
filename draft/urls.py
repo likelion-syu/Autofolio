5a396782d9c2b56033212fa944c3ef29722ded3f
seonghyeon <tjdgusdkf@gmail.com>
2019-08-09 19:58:59 +0900

    index gogo

--- draft/urls.py
+++ draft/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('new/', views.draft_new, name='draft_new'),
     path('crete/', views.draft_create, name='draft_create'),
     ##READ
-    path('list/', views.draft_list, name="draft_list"),
+    path('', views.draft_list, name="draft_list"),
 	path('detail/', views.draft_detail , name="draft_detail"),  # 오류파티
     ##UPDATE
     path('edit/<int:draft_id>/', views.draft_edit, name='draft_edit'),
